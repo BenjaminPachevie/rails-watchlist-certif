45574214896e66473b46325695a4a049b20be598
Benjamin Pachevie <b.pachevie@gmail.com>
2023-09-14 11:54:34 +0200

    updating model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = 
